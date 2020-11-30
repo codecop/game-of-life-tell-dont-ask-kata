d4693655ca5f80893290a5225f861447bced62df
kbayrhammer <klaus.bayrhammer@redbull.com>
2020-11-30 11:03:14 +0100

    cont living neighbors

--- src/rules.test.ts
+++ src/rules.test.ts
@@ -1,4 +1,4 @@
-import { expect } from 'chai';
+import {expect} from 'chai';
 
 function applyRules(cs: CellState, neighbourCount: number, cb: (nextCellState: CellState) => void) {
     // TODO finish, not related to TDA
@@ -9,12 +9,15 @@ enum CellState {
     Alive, Dead,
 }
 
-it('a cell without neighbours dies', (cb) => {
-    // 1. start rules
-    applyRules(CellState.Alive, 0, (nextCellState) => {
-        expect(nextCellState).to.equal(CellState.Dead);
-        cb();
+describe('rules', function () {
+    it('a cell without neighbours dies', (cb) => {
+        // 1. start rules
+        applyRules(CellState.Alive, 0, (nextCellState) => {
+            expect(nextCellState).to.equal(CellState.Dead);
+            cb();
+        });
     });
+    // TODO more tests, not related to TDA
 });
 
 class Cell {
@@ -31,13 +34,15 @@ class Cell {
 
 }
 
-it('a cell updates itself', (cb) => {
-    // 2. what is callback for rules
-    const cell = new Cell(CellState.Alive);
-    cell.update(CellState.Dead);
-    cell.print((nextCellState) => {
-        expect(nextCellState).to.equal(CellState.Dead);
-        cb();
+describe('cell', function () {
+    it('a cell updates itself', (cb) => {
+        // 2. what is callback for rules
+        const cell = new Cell(CellState.Alive);
+        cell.update(CellState.Dead);
+        cell.print((nextCellState) => {
+            expect(nextCellState).to.equal(CellState.Dead);
+            cb();
+        });
     });
 });
 
@@ -45,7 +50,16 @@ class Grid {
     private cell: Cell | null = null;
 
     public countLivingNeighboursAt(x: number, y: number, cb: (neighboursCount: number) => void) {
-        cb(0);
+        if(this.cell == null) {
+            cb(0);
+            return;
+        }
+
+        this.cell.print(cellState => {
+            const neighboursCount = cellState === CellState.Alive ? 1 : 0;
+            cb(neighboursCount);
+        })
+
     }
 
     public put(x: number, y: number, cell: Cell) {
@@ -59,25 +73,38 @@ class Grid {
     }
 }
 
-it('counts neighbours', (cb) => {
-    // 3. where do neighbours come from
-    const grid = new Grid();
+describe('grid', function () {
 
-    grid.countLivingNeighboursAt(0, 0, (neighboursCount: number) => {
-        expect(neighboursCount).to.equal(0);
-        cb();
+    it('counts neighbours in empty grid', (cb) => {
+        // 3. where do neighbours come from
+        const grid = new Grid();
+
+        grid.countLivingNeighboursAt(1, 1, (neighboursCount: number) => {
+            expect(neighboursCount).to.equal(0);
+            cb();
+        });
     });
-});
+    it('count alive neighbors not in corner', (cb) => {
+        // 3.5. where do neighbours come from
+        const grid = new Grid();
+        grid.put(0,0, new Cell(CellState.Alive))
 
-// 4. what is callback of countNeighboursAt
+        grid.countLivingNeighboursAt(1, 1, (neighboursCount: number) => {
+            expect(neighboursCount).to.equal(1);
+            cb();
+        });
+    });
 
-it('grid should contain cells', (cb) => {
-    const grid = new Grid();
-    const cell = new Cell(CellState.Alive);
-    grid.put(0, 0, cell);
 
-    grid.eachCell((c) => {
-        expect(c).to.deep.equal(cell);
-        cb();
+    it('grid should contain cells', (cb) => {
+        // 4. what is callback of countNeighboursAt
+        const grid = new Grid();
+        const cell = new Cell(CellState.Alive);
+        grid.put(0, 0, cell);
+
+        grid.eachCell((c) => {
+            expect(c).to.deep.equal(cell);
+            cb();
+        });
     });
 });
